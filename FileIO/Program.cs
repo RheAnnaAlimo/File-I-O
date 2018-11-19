1a1bf509d201c5c30c629070d44ef37100e78fc5
RHEANNA ALIMO <rheanna.alimo@outlook.com>
2018-11-18 19:00:01 -0800

    Add project files.

---
+++ FileIO/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIO
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string path = File.ReadAllText("C:\\Devil.txt");
+
+            Console.WriteLine(path); // display file on console
+
+            Console.WriteLine("Characters: " + path.Length); // total characters
+
+            Console.WriteLine("Total Lines (with whitespace) : " + (path.Split('\n').Length));  // total lines
+
+            Console.WriteLine("Total Words: " + path.Split(' ').Length); // total words
+
+            int count = 0;
+
+            if (string.IsNullOrEmpty(path) == true)
+            {
+                count++;
+                Console.WriteLine(count);
+            }
+
+        }
+    }
+}
+
+
+  
